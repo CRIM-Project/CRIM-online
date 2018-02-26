71f18f0d73b55975195bca7a622b5d6ce85ee18b
Micah Walter <micahjwalter@gmail.com>
2018-02-26 13:46:58 -0500

    make database settings hidden: import module with correct name

--- crim/settings.py
+++ crim/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from settings_production import *
+from crim.settings_production import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
