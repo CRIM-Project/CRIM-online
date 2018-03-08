fb7c9b8015e78164d4b126bda69f723d72f14f36
Micah Walter <micahjwalter@gmail.com>
2018-03-07 23:15:06 -0500

    admin static files

--- crim/settings.py
+++ crim/settings.py
@@ -120,4 +120,4 @@ APPEND_SLASH = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'crim/static')
