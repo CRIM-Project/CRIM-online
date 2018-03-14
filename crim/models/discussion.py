2cd5627bbaeed6a9a2b90fcce40e61eb4164b998
Micah Walter <micahjwalter@gmail.com>
2018-03-14 19:03:47 -0400

    Remove generic keys from comment, discussion, note

--- crim/models/discussion.py
+++ crim/models/discussion.py
@@ -16,10 +16,6 @@ class CRIMDiscussion(models.Model):
         related_name='discussions',
     )
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.CharField(max_length=16)
-    content_object = GenericForeignKey('content_type', 'object_id')
-
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
