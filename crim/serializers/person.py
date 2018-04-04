563e6cdcb7b51f3922527f97f084c346f3b0cd2d
Micah Walter <micahjwalter@gmail.com>
2018-04-04 18:39:01 -0400

    Check to see if role_type actually exists

--- crim/serializers/person.py
+++ crim/serializers/person.py
@@ -3,37 +3,33 @@ from crim.models.role import CRIMRole
 from rest_framework import serializers
 
 
-class CRIMPersonListSerializer(serializers.HyperlinkedModelSerializer):
-    def get_roles(self, obj):
+class CRIMPersonSerializer(serializers.HyperlinkedModelSerializer):
+    def get_unique_roles(self, obj):
         unique_roles = []
         crimroles = CRIMRole.objects.filter(person=obj)
         for crimrole in crimroles:
-            role_type_name = crimrole.role_type.name
-            if role_type_name not in unique_roles:
-                unique_roles.append(role_type_name)
+            if crimrole.role_type:
+                role_type_name = crimrole.role_type.name
+                if role_type_name not in unique_roles:
+                    unique_roles.append(role_type_name)
         unique_roles.sort()
         return unique_roles
 
-    roles = serializers.SerializerMethodField()
+    unique_roles = serializers.SerializerMethodField()
+
+    roles = serializers.ReadOnlyField()
 
     class Meta:
         model = CRIMPerson
-        fields = '__all__'
-
-
-class CRIMPersonDetailSerializer(serializers.HyperlinkedModelSerializer):
-    def get_roles(self, obj):
-        unique_roles = []
-        crimroles = CRIMRole.objects.filter(person=obj)
-        for crimrole in crimroles:
-            role_type_name = crimrole.role_type.name
-            if role_type_name not in unique_roles:
-                unique_roles.append(role_type_name)
-        unique_roles.sort()
-        return unique_roles
-
-    roles = serializers.SerializerMethodField()
-
-    class Meta:
-        model = CRIMPerson
-        fields = '__all__'
+        fields = (
+            'url',
+            'name',
+            'name_sort',
+            'name_alternate_list',
+            'birth_date',
+            'death_date',
+            'active_date',
+            'remarks',
+            'roles',
+            'unique_roles',
+        )
