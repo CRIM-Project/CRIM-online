175858eb7ac22d92069a1da93102cfe735d253ee
Micah Walter <micahjwalter@gmail.com>
2018-04-10 09:22:49 -0400

    Flat pages now working

--- crim/settings.py
+++ crim/settings.py
@@ -49,11 +49,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'crim.urls'
@@ -123,6 +124,8 @@ LOGOUT_REDIRECT_URL = '/'
 
 # Make it acceptable for users to leave off a slash in URLs
 APPEND_SLASH = True
+
+SITE_ID = 2
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
