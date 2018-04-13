b58920f5553ffb1ce4aa7255ade8b76b79649f93
Micah Walter <micahjwalter@gmail.com>
2018-04-13 12:02:55 -0400

    use common get_date_sort function, rather than rewriting for each model

--- crim/models/person.py
+++ crim/models/person.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.utils.text import slugify
 from django.contrib.postgres.fields import ArrayField
 from django.utils.html import escape
+from crim.common import get_date_sort
 import re
-
-from dateutil.parser import parse
 
 
 class CRIMPerson(models.Model):
@@ -15,7 +14,7 @@ class CRIMPerson(models.Model):
         ordering = ['name_sort']
 
     # Null should be False, because blank values should be stored as empty string.
-    person_id = models.SlugField(max_length=64, unique=True, db_index=True)
+    person_id = models.CharField(max_length=32, unique=True, db_index=True)
     name = models.CharField(max_length=64, db_index=True)
     name_sort = models.CharField('sort name (such as ‘Lassus, Orlande de’)', max_length=64, blank=True, db_index=True)
     # Tried using an array, it was much more trouble than it was worth
@@ -49,21 +48,6 @@ class CRIMPerson(models.Model):
             num += 1
         return unique_slug
 
-    def _get_date_sort(self):
-        try:
-            birth_date_parsed = parse(self.birth_date.replace('x', '0'), fuzzy=True).year
-        except ValueError:
-            birth_date_parsed = 0
-        try:
-            death_date_parsed = parse(self.death_date.replace('x', '0'), fuzzy=True).year
-        except ValueError:
-            death_date_parsed = 0
-        try:
-            active_date_parsed = parse(self.active_date.replace('x', '0'), fuzzy=True).year
-        except ValueError:
-            active_date_parsed = 0
-        return max(birth_date_parsed, death_date_parsed, active_date_parsed)
-
     def save(self, *args, **kwargs):
         # Create unique person_id based on the name
         if not self.person_id:
@@ -77,10 +61,11 @@ class CRIMPerson(models.Model):
             self.name_sort = self.name
 
         # Add sortable date field based on birth, death and active dates
-        if self._get_date_sort() == 0:
+        dates = [self.birth_date, self.death_date, self.active_date]
+        if get_date_sort(dates) == 0:
             self.date_sort = None
         else:
-            self.date_sort = self._get_date_sort()
+            self.date_sort = get_date_sort(dates)
 
         # Remove extraneous newlines
         self.name_alternate_list = re.sub(r'[\n\r]+', r'\n', self.name_alternate_list)
