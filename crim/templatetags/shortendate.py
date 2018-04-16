a76ceacbeff5f4dbdea1526764a3a7e64b8325d1
Micah Walter <micahjwalter@gmail.com>
2018-04-16 16:20:46 -0400

    Don't display full date

---
+++ crim/templatetags/shortendate.py
@@ -0,0 +1,12 @@
+from django.template.defaultfilters import register
+
+from crim.common import get_date_sort
+
+
+@register.filter(name='shortendate')
+def shortendate(text):
+    date_sort = get_date_sort([text])
+    if '?' in text:
+        return str(date_sort) + '?'
+    else:
+        return str(date_sort)
