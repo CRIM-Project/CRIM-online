22999201a265502fff7ca912640cdb8a7a51e0d3
Micah Walter <micahjwalter@gmail.com>
2018-05-21 12:36:02 -0400

    allow firstitem template tag to work for strings as well as lists

--- crim/templatetags/firstitem.py
+++ crim/templatetags/firstitem.py
@@ -2,7 +2,10 @@ from django.template.defaultfilters import register
 
 
 @register.filter(name='firstitem')
-def firstitem(s, delimiter='\n'):
+def firstitem(items, delimiter='\n'):
     '''Returns only the first item in a list.'''
-    l = s.split(delimiter)
-    return l[0] if l else ''
+    if isinstance(items, str):
+        l = items.split(delimiter)
+        return l[0] if l else ''
+    else:
+        return items[0]
