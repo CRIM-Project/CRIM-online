e54f36f34e270473421ef82a807716adaa63e6e6
Micah Walter <micahjwalter@gmail.com>
2018-05-30 17:49:06 -0400

    Added html views for genre list and genre detail

--- crim/serializers/genre.py
+++ crim/serializers/genre.py
@@ -1,9 +1,104 @@
 from crim.models.genre import CRIMGenre
+from crim.models.mass import CRIMMass
+from crim.models.person import CRIMPerson
+from crim.models.piece import CRIMPiece
+from crim.models.role import CRIMRole, CRIMRoleType
 from rest_framework import serializers
 
 
-class CRIMGenreSerializer(serializers.HyperlinkedModelSerializer):
+class CRIMRoleTypeGenreSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='crimroletype-detail-data', lookup_field='role_type_id')
+
+    class Meta:
+        model = CRIMRoleType
+        fields = (
+            'url',
+            'name',
+        )
+
+
+class CRIMPersonGenreSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='crimperson-detail-data', lookup_field='person_id')
+
+    class Meta:
+        model = CRIMPerson
+        fields = (
+            'url',
+            'name',
+        )
+
+
+class CRIMRoleGenreSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='crimrole-detail-data', lookup_field='pk')
+    person = CRIMPersonGenreSerializer(read_only=True)
+    role_type = CRIMRoleTypeGenreSerializer(read_only=True)
+
+    class Meta:
+        model = CRIMRole
+        fields = (
+            'url',
+            'person',
+            'role_type',
+            'date',
+            'remarks',
+        )
+
+
+class CRIMMassGenreSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='crimmass-detail-data',
+        lookup_field='mass_id',
+    )
+    roles = CRIMRoleGenreSerializer(
+        many=True,
+        read_only=True,
+        source='roles_as_mass',
+    )
+
+    class Meta:
+        model = CRIMMass
+        fields = (
+            'url',
+            'mass_id',
+            'title',
+            'roles',
+        )
+
+
+class CRIMPieceGenreSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='crimpiece-detail-data', lookup_field='piece_id')
+    roles = CRIMRoleGenreSerializer(
+        many=True,
+        read_only=True,
+        source='roles_as_piece',
+    )
+    mass = CRIMMassGenreSerializer(read_only=True)
+    pdf_links = serializers.SerializerMethodField()
+    mei_links = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CRIMPiece
+        fields = (
+            'url',
+            'piece_id',
+            'title',
+            'mass',
+            'roles',
+            'pdf_links',
+            'mei_links',
+            'remarks',
+        )
+
+    def get_pdf_links(self, obj):
+        return obj.pdf_links.split('\n')
+
+    def get_mei_links(self, obj):
+        return obj.mei_links.split('\n')
+
+
+class CRIMGenreListSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='crimgenre-detail-data', lookup_field='genre_id')
+    number_of_pieces = serializers.SerializerMethodField()
 
     class Meta:
         model = CRIMGenre
@@ -11,4 +106,26 @@ class CRIMGenreSerializer(serializers.HyperlinkedModelSerializer):
             'url',
             'name',
             'remarks',
+            'number_of_pieces',
+        )
+
+    def get_number_of_pieces(self, obj):
+        pieces = CRIMPiece.objects.filter(genre=obj)
+        return len(pieces)
+
+
+class CRIMGenreDetailSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='crimgenre-detail-data', lookup_field='genre_id')
+    pieces = CRIMPieceGenreSerializer(
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = CRIMGenre
+        fields = (
+            'url',
+            'name',
+            'remarks',
+            'pieces',
         )
--- crim/views/genre.py
+++ crim/views/genre.py
@@ -4,8 +4,11 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework import permissions
 
 from crim.renderers.custom_html_renderer import CustomHTMLRenderer
-from crim.serializers.genre import CRIMGenreSerializer
+from crim.serializers.genre import CRIMGenreListSerializer, CRIMGenreDetailSerializer
 from crim.models.genre import CRIMGenre
+from crim.common import earliest_date
+
+COMPOSER = 'Composer'
 
 
 class GenreListHTMLRenderer(CustomHTMLRenderer):
@@ -18,6 +21,33 @@ class GenreListHTMLRenderer(CustomHTMLRenderer):
 
 class GenreDetailHTMLRenderer(CustomHTMLRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
+        for piece in data['pieces']:
+            # - Add `composer` field to content: only look at roles with
+            # the role type with name "Composer", and add all such names
+            # to the list, along with the url of the composer
+            # - Add `date` field to content: again, only look at roles
+            # with the role type "Composer"
+            composers = []
+            dates = []
+            for role in piece['roles']:
+                if role['role_type'] and role['role_type']['name'] == COMPOSER:
+                    composer_html = ('<a href="' + role['person']['url'].replace('/data/', '/') +
+                                     '">' + role['person']['name'] + '</a>')
+                    composers.append(composer_html)
+                    if role['date']:
+                        dates.append(role['date'])
+            if piece['mass']:
+                for role in piece['mass']['roles']:
+                    if role['role_type'] and role['role_type']['name'] == COMPOSER:
+                        composer_html = ('<a href="' + role['person']['url'].replace('/data/', '/') +
+                                         '">' + role['person']['name'] + '</a>')
+                        composers.append(composer_html)
+                        if role['date']:
+                            dates.append(role['date'])
+            piece['composers_with_url'] = '; '.join(composers) if composers else '-'
+            # Only add one composer's date for clarity, choosing the earliest.
+            piece['date'] = earliest_date(dates)
+
         template_names = ['genre/genre_detail.html']
         template = self.resolve_template(template_names)
         context = self.get_template_context({'content': data}, renderer_context)
@@ -27,8 +57,11 @@ class GenreDetailHTMLRenderer(CustomHTMLRenderer):
 class GenreList(generics.ListAPIView):
     model = CRIMGenre
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    serializer_class = CRIMGenreSerializer
-    renderer_classes = (JSONRenderer,)  # add html later
+    serializer_class = CRIMGenreListSerializer
+    renderer_classes = (
+        GenreListHTMLRenderer,
+        JSONRenderer,
+    )
 
     def get_queryset(self):
         order_by = self.request.GET.get('order_by', 'genre_id')
@@ -38,7 +71,7 @@ class GenreList(generics.ListAPIView):
 class GenreListData(generics.ListAPIView):
     model = CRIMGenre
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    serializer_class = CRIMGenreSerializer
+    serializer_class = CRIMGenreListSerializer
     renderer_classes = (JSONRenderer,)
 
     def get_queryset(self):
@@ -49,8 +82,11 @@ class GenreListData(generics.ListAPIView):
 class GenreDetail(generics.RetrieveAPIView):
     model = CRIMGenre
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    serializer_class = CRIMGenreSerializer
-    renderer_classes = (JSONRenderer,)  # add html later
+    serializer_class = CRIMGenreDetailSerializer
+    renderer_classes = (
+        GenreDetailHTMLRenderer,
+        JSONRenderer,
+    )
     queryset = CRIMGenre.objects.all()
 
     def get_object(self):
@@ -67,7 +103,7 @@ class GenreDetail(generics.RetrieveAPIView):
 class GenreDetailData(generics.RetrieveAPIView):
     model = CRIMGenre
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    serializer_class = CRIMGenreSerializer
+    serializer_class = CRIMGenreDetailSerializer
     renderer_classes = (JSONRenderer,)
     queryset = CRIMGenre.objects.all()
 
