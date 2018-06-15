13faab5db4fbf5ec55d85a73766f2a12f3696e7c
Micah Walter <micahjwalter@gmail.com>
2018-06-15 12:05:58 -0400

    Finish fields for indexing pieces

--- crim/search_indexes.py
+++ crim/search_indexes.py
@@ -4,9 +4,12 @@ from crim.models.piece import CRIMPiece
 
 class CRIMPieceIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    composer = indexes.CharField(use_template=True, faceted=True)
-    # author = indexes.CharField(model_attr='user')
-    # pub_date = indexes.DateTimeField(model_attr='pub_date')
+    piece_id = indexes.CharField(model_attr='piece_id', faceted=True)
+    title = indexes.CharField(model_attr='title', faceted=True)
+    mass = indexes.CharField(model_attr='mass__title', faceted=True)
+    composer = indexes.CharField(model_attr='composer__name', faceted=True)
+    genre = indexes.CharField(model_attr='genre__name', faceted=True)
+    date = indexes.IntegerField(model_attr='date_sort', faceted=True)
 
     def get_model(self):
         return CRIMPiece
