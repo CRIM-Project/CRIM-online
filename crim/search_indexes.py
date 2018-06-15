1697f235563491cfca7a641958b292a0c0188673
Micah Walter <micahjwalter@gmail.com>
2018-06-15 12:15:20 -0400

    Allow nulls on some fields, don't facet some fields

--- crim/search_indexes.py
+++ crim/search_indexes.py
@@ -4,12 +4,12 @@ from crim.models.piece import CRIMPiece
 
 class CRIMPieceIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    piece_id = indexes.CharField(model_attr='piece_id', faceted=True)
-    title = indexes.CharField(model_attr='title', faceted=True)
-    mass = indexes.CharField(model_attr='mass__title', faceted=True)
-    composer = indexes.CharField(model_attr='composer__name', faceted=True)
+    piece_id = indexes.CharField(model_attr='piece_id')
+    title = indexes.CharField(model_attr='title')
+    mass = indexes.CharField(model_attr='mass__title', null=True)
+    composer = indexes.CharField(model_attr='composer__name', faceted=True, null=True)
     genre = indexes.CharField(model_attr='genre__name', faceted=True)
-    date = indexes.IntegerField(model_attr='date_sort', faceted=True)
+    date = indexes.IntegerField(model_attr='date_sort', faceted=True, null=True)
 
     def get_model(self):
         return CRIMPiece
