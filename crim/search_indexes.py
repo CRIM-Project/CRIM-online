24a8ad1b561ac9811414189d7245857e1a03e929
Micah Walter <micahjwalter@gmail.com>
2018-06-25 14:05:09 -0400

    faceted fields: observer, rt_q

--- crim/search_indexes.py
+++ crim/search_indexes.py
@@ -1,28 +1,28 @@
 from haystack import indexes
-from crim.models.piece import CRIMPiece
+# from crim.models.piece import CRIMPiece
 from crim.models.relationship import CRIMRelationship
 
 
-class CRIMPieceIndex(indexes.SearchIndex, indexes.Indexable):
-    '''This indexes all the CRIMPiece objects. However, it is
-    not used in production because we search for CRIMRelationship
-    objects instead. It was used as a simpler text case and
-    is left here in case searching for pieces directly ends
-    up being useful.
-    '''
-    text = indexes.CharField(document=True, use_template=True)
-    piece_id = indexes.CharField(model_attr='piece_id')
-    title = indexes.CharField(model_attr='title')
-    mass_title = indexes.CharField(model_attr='mass__title', null=True)
-    composer = indexes.CharField(model_attr='composer__name_sort', faceted=True, null=True)
-    genre = indexes.CharField(model_attr='genre__name', faceted=True)
-    date = indexes.IntegerField(model_attr='date_sort', faceted=True, null=True)
-    pdf_links = indexes.CharField(model_attr='pdf_links', null=True, indexed=False)
-    mei_links = indexes.CharField(model_attr='mei_links', null=True, indexed=False)
-    remarks = indexes.CharField(model_attr='remarks', null=True, indexed=False)
-
-    def get_model(self):
-        return CRIMPiece
+# class CRIMPieceIndex(indexes.SearchIndex, indexes.Indexable):
+#     '''This indexes all the CRIMPiece objects. However, it is
+#     not used in production because we search for CRIMRelationship
+#     objects instead. It was used as a simpler text case and
+#     is left here in case searching for pieces directly ends
+#     up being useful.
+#     '''
+#     text = indexes.CharField(document=True, use_template=True)
+#     piece_id = indexes.CharField(model_attr='piece_id')
+#     title = indexes.CharField(model_attr='title')
+#     mass_title = indexes.CharField(model_attr='mass__title', null=True)
+#     composer = indexes.CharField(model_attr='composer__name_sort', faceted=True, null=True)
+#     genre = indexes.CharField(model_attr='genre__name', faceted=True)
+#     date = indexes.IntegerField(model_attr='date_sort', faceted=True, null=True)
+#     pdf_links = indexes.CharField(model_attr='pdf_links', null=True, indexed=False)
+#     mei_links = indexes.CharField(model_attr='mei_links', null=True, indexed=False)
+#     remarks = indexes.CharField(model_attr='remarks', null=True, indexed=False)
+#
+#     def get_model(self):
+#         return CRIMPiece
 
 
 class CRIMRelationshipIndex(indexes.SearchIndex, indexes.Indexable):
@@ -32,7 +32,8 @@ class CRIMRelationshipIndex(indexes.SearchIndex, indexes.Indexable):
 
     # Information about the CRIMRelationship object itself
 
-    observer_person_id = indexes.CharField(model_attr='observer__person_id', faceted=True, null=False)
+    relationship_id = indexes.IntegerField(model_attr='id', null=False)
+    observer_person_id = indexes.CharField(model_attr='observer__person_id', null=False)
     observer_name_sort = indexes.CharField(model_attr='observer__name_sort', faceted=True, null=False)
 
     rt_q = indexes.BooleanField(model_attr='rt_q', faceted=True)
