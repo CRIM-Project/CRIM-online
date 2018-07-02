9e65d471c6eca9a7daa6b39277650416041804fb
Micah Walter <micahjwalter@gmail.com>
2018-07-02 12:16:17 -0400

    indexing with solr

--- crim/views/search.py
+++ crim/views/search.py
@@ -22,3 +22,17 @@ def _empty_search(request):
         'derivative_genre': sorted(facets['derivative_genre']),
     }
     return render(request, 'search/search.html', data)
+def _empty_search(request):
+    s = CRIMSolrSearch(request)
+    ret = s.facets(fq=['crim_relationship'], rows=0)
+    facets = ret.facet_counts['facet_fields']
+    print(sorted(facets['observer']))
+
+    data = {
+        'observer': facets['observer'],
+        'model_composer': facets['model_composer'],
+        'model_genre': facets['model_genre'],
+        'derivative_composer': facets['derivative_composer'],
+        'derivative_genre': facets['derivative_genre'],
+    }
+    return render(request, 'search/search.html', data)
