ad5669a9ba2280d41f3df49d5097b01cad31339f
Micah Walter <micahjwalter@gmail.com>
2018-07-02 16:41:24 -0400

    blocks for observer, composer, genre, and relationship types

--- crim/templatetags/dictkey.py
+++ crim/templatetags/dictkey.py
@@ -1,7 +1,7 @@
 from django.template.defaultfilters import register
 
 
-@register.filter(name='dict_key')
-def dict_key(d, k):
+@register.filter(name='dictkey')
+def dictkey(d, k):
     '''Returns the given key from a dictionary.'''
     return d.get(k)
