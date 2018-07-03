6332122c0c3ff88f1b3c94fcb7956aba0c5bd8cd
Micah Walter <micahjwalter@gmail.com>
2018-07-03 15:34:35 -0400

    Bugfix for emptying solr index

--- solr_empty.py
+++ solr_empty.py
@@ -3,6 +3,6 @@ import sys
 if __name__ == '__main__':
     print('Emptying Solr')
     solrconn = solr.SolrConnection('http://localhost:8983/solr/crim')
-    solrconn.delete_query('*:')
+    solrconn.delete_query('*:*')
     solrconn.commit()
     sys.exit()
