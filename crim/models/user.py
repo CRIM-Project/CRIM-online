01f619252739e606868f7476a087f3bed6fe68fa
Micah Walter <micahjwalter@gmail.com>
2018-08-18 16:28:04 -0400

    Expanded and regularized infrastructure for displaying user and userprofile

--- crim/models/user.py
+++ crim/models/user.py
@@ -5,12 +5,16 @@ from django.contrib.auth.models import User
 class CRIMUserProfile(models.Model):
     class Meta:
         app_label = 'crim'
+        ordering = ['user__last_name', 'user__first_name', 'user__username']
 
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
         db_index=True,
     )
+
+    name = models.CharField(max_length=64, db_index=True, blank=True)
+    name_sort = models.CharField('sort name (such as ‘Lassus, Orlande de’)', max_length=64, blank=True, db_index=True)
 
     person = models.ForeignKey(
         'CRIMPerson',
@@ -22,8 +26,33 @@ class CRIMUserProfile(models.Model):
         related_name='profile',
     )
 
+    @property
+    def username(self):
+        return self.user.username
+
+    @property
+    def username_with_name(self):
+        if self.name:
+            return '{0} ({1})'.format(self.user.username, self.name)
+        else:
+            return self.user.username
+
     def __str__(self):
-        return '{0} {1}'.format(self.user.first_name, self.user.last_name)
+        if self.name:
+            return self.name
+        else:
+            return self.user.username
+
+    def save(self):
+        if self.person:
+            self.name = self.person.name
+            self.name_sort = self.person.name_sort
+        else:
+            if not self.name:
+                self.name = (self.user.first_name + ' ' + self.user.last_name).strip()
+            if not self.name_sort:
+                self.name_sort = (self.user.last_name + ' ' + self.user.first_name).strip()
+        super().save()
 
 
 User.profile = property(lambda u: CRIMUserProfile.objects.get_or_create(user=u)[0])
--- crim/views/user.py
+++ crim/views/user.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.shortcuts import get_object_or_404
 
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.authtoken.models import Token
+from rest_framework.renderers import JSONRenderer
 
-from crim.serializers.user import UserSerializer, UserListSerializer
+from crim.serializers.user import UserSerializer, UserListSerializer, CRIMUserProfileDetailSerializer
+from crim.models.user import CRIMUserProfile
 
 
 class UserList(generics.ListCreateAPIView):
@@ -28,6 +31,28 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         queryset = User.objects.all()
         return queryset
+
+
+class UserProfile(generics.RetrieveAPIView):
+    model = CRIMUserProfile
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    serializer_class = CRIMUserProfileDetailSerializer
+    renderer_classes = (
+        JSONRenderer,
+    )
+    queryset = CRIMUserProfile.objects.all()
+
+    def get_object(self):
+        url_arg = self.kwargs['username']
+        user = CRIMUserProfile.objects.filter(user__username=url_arg)
+
+        obj = get_object_or_404(user)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
+class UserProfileData(UserProfile):
+    pass
 
 
 @receiver(post_save, sender=User)
