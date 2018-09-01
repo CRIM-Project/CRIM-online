c13a4727bf395f616bb3e67b13dbc4f298d6706f
Micah Walter <micahjwalter@gmail.com>
2018-09-01 17:35:23 -0400

    Ability to see comments and list and edit one's own

--- crim/views/comment.py
+++ crim/views/comment.py
@@ -1,7 +1,9 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from rest_framework import generics
-from rest_framework.renderers import JSONRenderer
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework.views import APIView
 
 from crim.renderers.custom_html_renderer import CustomHTMLRenderer
 from crim.serializers.comment import CRIMCommentSerializer
@@ -38,25 +40,22 @@ class CommentList(generics.ListAPIView):
         return CRIMComment.objects.order_by(order_by)
 
 
-class CommentDetail(generics.RetrieveAPIView):
-    model = CRIMComment
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    serializer_class = CRIMCommentSerializer
-    renderer_classes = (
-        CommentDetailHTMLRenderer,
-        JSONRenderer,
-    )
-    queryset = CRIMComment.objects.all()
+class CommentDetail(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'comment/comment_detail.html'
 
-    def get_object(self):
-        url_arg = self.kwargs['comment_id']
-        comment = CRIMComment.objects.filter(comment_id=url_arg)
-        if not comment.exists():
-            comment = CRIMComment.objects.filter(name__iexact=url_arg)
+    def get(self, request, comment_id):
+        comment = get_object_or_404(CRIMComment, comment_id=comment_id)
+        serializer = CRIMCommentSerializer(comment, context={'request': request})
+        return Response({'serializer': serializer, 'content': comment})
 
-        obj = get_object_or_404(comment)
-        self.check_object_permissions(self.request, obj)
-        return obj
+    def post(self, request, comment_id):
+        comment = get_object_or_404(CRIMComment, comment_id=comment_id)
+        serializer = CRIMCommentSerializer(comment, data=request.data, context={'request': request})
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, 'content': comment})
+        serializer.save()
+        return redirect('crimcomment-list')
 
 
 class CommentListData(CommentList):
