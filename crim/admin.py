8ef049e5845ac106b42857bf6c42665398276726
Micah Walter <micahjwalter@gmail.com>
2019-03-12 17:07:31 -0400

    Include original name in voice admin

--- crim/admin.py
+++ crim/admin.py
@@ -426,6 +426,7 @@ class CRIMVoiceAdmin(admin.ModelAdmin):
         'voice_id',
         'piece_title',
         'order',
+        'original_name',
         'regularized_name',
     )
     ordering = (
