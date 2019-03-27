f3a323fc50a395d9ec4106a674291b25bd496d69
Ian Fisher <iafisher@protonmail.com>
2019-03-27 10:31:52 -0400

    Implement mechanism to post top-level comments

--- crim/views/forum.py
+++ crim/views/forum.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
 from ..forms import ForumPostForm
-from ..models.forum import ForumPost
+from ..models.forum import ForumComment, ForumPost
 from ..models.user import CRIMUserProfile
 
 
@@ -24,6 +24,25 @@ def create_post(request):
     return render(request, "forum/create_post.html", {"form": form})
 
 
+@login_required
+def create_comment(request):
+    if request.method == "POST":
+        pk = int(request.POST["post"])
+        post = ForumPost.objects.get(pk=pk)
+        crim_user = CRIMUserProfile.objects.get(user=request.user)
+        ForumComment.objects.create(
+            text=request.POST["body"],
+            parent=None,
+            post=post,
+            user=crim_user,
+        )
+        return redirect("view_forum_post", pk)
+    else:
+        return redirect("home")
+
+
 def view_post(request, pk):
     post = get_object_or_404(ForumPost, pk=pk)
-    return render(request, "forum/view_post.html", {"post": post})
+    comments = post.forumcomment_set.all()
+    context = {"comments": comments, "post": post}
+    return render(request, "forum/view_post.html", context)
