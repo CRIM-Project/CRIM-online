7544211ab64c274687da52ac06a46b5b63346f36
Ian Fisher <iafisher@protonmail.com>
2019-04-12 11:58:48 -0400

    Enforce that comments and posts may not be empty/all whitespace

--- crim/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class ForumPostForm(forms.Form):
-    title = forms.CharField(label="Title", max_length=100)
-    body = forms.CharField(label="Body", widget=forms.Textarea)
