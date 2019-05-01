8c90cb4f1bb356d11fbbd207a567222d2ede1bc0
Ian Fisher <iafisher@protonmail.com>
2019-05-01 09:46:23 -0400

    Auto-insert links for CRIM model IDs in forum posts and comments

--- crim/views/forum.py
+++ crim/views/forum.py
@@ -1,4 +1,5 @@
 import html
+import re
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
@@ -72,8 +73,16 @@ def create_or_reply_comment(post_pk, comment_pk, request):
 
 def view_post(request, pk):
     post = get_object_or_404(CRIMForumPost, pk=pk)
+    # VERY IMPORTANT: escape any non-literal text that may contain HTML!
+    post_title = insert_links(html.escape(post.title))
+    post_text = insert_links(html.escape(post.text))
     comment_tree = render_comment_tree(post.crimforumcomment_set.filter(parent=None))
-    context = {"comment_tree": comment_tree, "post": post}
+    context = {
+        "comment_tree": comment_tree,
+        "post": post,
+        "post_title": post_title,
+        "post_text": post_text,
+    }
     return render(request, "forum/view_post.html", context)
 
 
@@ -89,14 +98,15 @@ def render_comment_tree(comment_set):
 
 
 def render_comment(comment):
-    # VERY IMPORTANT: escape any non-literal text that may contain HTML!
     if comment.user:
         user = comment.user.name + " (" + comment.user.user.username + ")"
     else:
         user = "[deleted]"
+    # VERY IMPORTANT: escape any non-literal text that may contain HTML!
     user = html.escape(user)
 
     text = html.escape(comment.text)
+    text = insert_links(text)
     base = "<li><h2>{} at {}</h2><p>{}</p><p><a href=\"{}\">reply</a></p></li>".format(
         user,
         comment.created_at.strftime("%I:%M %p on %B %d, %Y"),
@@ -104,3 +114,19 @@ def render_comment(comment):
         reverse("reply_forum_comment", args=[comment.pk]),
     )
     return base + render_comment_tree(comment.crimforumcomment_set.all())
+
+
+_link_regex = re.compile(r"CRIM_Model_[0-9]{4}", re.IGNORECASE)
+def insert_links(text):
+    """Detect occurrences of piece IDs in the text, and insert HTML links."""
+    return _link_regex.sub(lambda m: create_link(m.group(0)), text)
+
+
+def create_link(piece_id):
+    """Given a piece ID, return an HTML link."""
+    normalized_piece_id = "CRIM_Model_" + piece_id[-4:]
+    piece = CRIMPiece.objects.get(piece_id=normalized_piece_id)
+    if piece is not None:
+        return '<a href="{}">{}</a>'.format(piece.get_absolute_url(), piece_id)
+    else:
+        return piece_id
