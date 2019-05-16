3842676788c23a62f6b20f499ec08c0bd1bd9989
Micah Walter <micahjwalter@gmail.com>
2019-05-16 19:09:13 -0400

    Use re_path in urls

--- crim/urls.py
+++ crim/urls.py
@@ -135,19 +135,19 @@ if 'django.contrib.admin' in settings.INSTALLED_APPS:
         re_path(r'^data/users/(?P<username>[0-9a-zA-Z_@+\.-]+)/$', UserProfileData.as_view(), name='crimuserprofile-detail-data'),
         re_path(r'^data/voices/$', VoiceListData.as_view(), name='crimvoice-list-data'),
         re_path(r'^data/voices/(?P<voice_id>[-_A-Za-z0-9\(\)]+)/$', VoiceDetailData.as_view(), name='crimvoice-detail-data'),
-        path("forum/", forum_views.index, name="forum_index"),
-        path("forum/create", forum_views.create_post, name="create_forum_post"),
-        path("forum/post/<int:pk>", forum_views.view_post, name="view_forum_post"),
-        path("forum/related/<piece>", forum_views.related, name="forum_related"),
-        path(
-            "forum/post/<int:post_pk>/comment",
+        re_path(r'^forum/', forum_views.index, name='forum_index'),
+        re_path(r'^forum/create/', forum_views.create_post, name='create_forum_post'),
+        re_path(r'^forum/post/(?P<pk>[0-9]+>/$', forum_views.view_post, name='view_forum_post'),
+        re_path(r'^forum/related/(?P<piece>[-_A-Za-z0-9]+)/$', forum_views.related, name='forum_related'),
+        re_path(
+            r'^forum/post/(?P<post_pk>[0-9])/comment/$',
             forum_views.create_comment,
-            name="create_forum_comment",
+            name='create_forum_comment',
         ),
-        path(
-            "forum/reply/<int:pk>",
+        re_path(
+            r'^forum/reply/(?P<pk>[0-9])/$",
             forum_views.reply_comment,
-            name="reply_forum_comment",
+            name='reply_forum_comment',
         ),
     ]
 
