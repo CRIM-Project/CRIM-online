69332280bcd8d1367a056bf4d6d3a2b4330f00ed
Micah Walter <micahjwalter@gmail.com>
2019-06-19 10:36:49 -0400

    Install CORS

--- crim/settings.py
+++ crim/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.staticfiles',
     'bootstrap_pagination',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
