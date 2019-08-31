92e10063c4a3f45e97ae276734f171870e504d4a
Micah Walter <micahjwalter@gmail.com>
2019-08-31 14:56:06 -0400

    minor tweaks to dev; enhanced security in settings

--- crim/settings.py
+++ crim/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = '!si#%r2x)6yu*dlxw#rx#)1w1gm+1jkz+zwr&_w2-ldr*9js%d'
 
 ALLOWED_HOSTS = (
     '127.0.0.1',
-    '159.65.177.99',
-    '92.154.49.37',
     'crimproject.org',
     'dev.crimproject.org',
     'ema.crimproject.org',
