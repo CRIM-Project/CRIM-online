d846f195f627a988a0c440bd97172580d2752b12
Micah Walter <micahjwalter@gmail.com>
2019-11-08 11:53:32 -0500

    Add management command to cache all pages of a single observation

---
+++ crim/management/commands/cache_observation.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+
+from crim.models.observation import CRIMObservation
+from crim.views.observation import render_observation
+
+
+class Command(BaseCommand):
+    help = 'Caches 35 pages of a single observation.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('observation_ids', nargs='+', type=int)
+
+    def handle(self, *args, **kwargs):
+        for observation_id in kwargs['observation_ids']:
+            observation = CRIMObservation.objects.get(id=observation_id)
+            print('Caching first highlighted page of <{}>'.format(observation_id))
+            try:
+                render_observation(
+                        observation.id,
+                        observation.piece.piece_id,
+                        observation.ema,
+                        None,
+                    )
+            except:
+                print('ERROR caching first highlighted page of <{}>'.format(observation.id))
+        for observation_id in kwargs['observation_ids']:
+            observation = CRIMObservation.objects.get(id=observation_id)
+            print('Caching all pages of <{}>'.format(observation_id))
+            try:
+                for i in range(35):
+                    render_observation(
+                            observation.id,
+                            observation.piece.piece_id,
+                            observation.ema,
+                            i+1,
+                        )
+            except:
+                print('ERROR caching all pages of <{}>'.format(observation.id))
