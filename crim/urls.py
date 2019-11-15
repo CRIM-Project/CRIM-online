9c8fcd7b98bdb1c91d20fe6fda6a9901597ddd26
Micah Walter <micahjwalter@gmail.com>
2019-11-15 19:12:06 +0100

    Debugging on local

--- crim/urls.py
+++ crim/urls.py
@@ -150,3 +150,9 @@ if 'django.contrib.admin' in settings.INSTALLED_APPS:
     urlpatterns += [
         re_path('about', include('django.contrib.flatpages.urls')),
     ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('debug/', include(debug_toolbar.urls)),
+    ] + urlpatterns
