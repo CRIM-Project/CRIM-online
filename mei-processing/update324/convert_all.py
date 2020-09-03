cadfa2033094c936c43537b9cc6a1b62358d878a
raffazizzi <raffaeleviglianti@gmail.com>
2020-09-03 17:40:14 -0400

    Converting MEI based on folder content instead of metadata file

--- mei-processing/update324/convert_all.py
+++ mei-processing/update324/convert_all.py
@@ -1,15 +1,16 @@
 import convert324
 import argparse
+from os import listdir
 import os.path
-import csv
 
 MEI_LOC = '../../crim/static/mei/'
 MEI_OUT = 'output/'
-METADATA = 'data/metadata.csv'
+
+# More metadata to be found here:
+# https://sites.google.com/haverford.edu/crim-project/technical-team#h.fm1mz32ig3l5
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Convert all MEI files in CRIM-Online from v3 to v4.')
-  parser.add_argument('--metadata', default=METADATA, dest="metadata", help="metadata csv location")
   parser.add_argument('--mei', default=MEI_LOC, dest="mei", help="directory where MEI files are located")
   parser.add_argument('--output', default=MEI_OUT, dest="output", help="output directory")
   parser.add_argument('--meigarage', default=None, dest="garage", help="MEI Garage endpoint")
@@ -19,16 +20,14 @@ if __name__ == '__main__':
   if not os.path.exists(options.output):
     os.makedirs(options.output)
 
-  with open(options.metadata, newline='') as csvfile:    
-    metadata = csv.reader(csvfile)
-    for [i, row] in enumerate(metadata):
-      if i == 0 or row[0] == '': continue
-      mei_filename = row[0]
-      mei = os.path.join(options.mei, mei_filename)
-      if (not os.path.exists(mei)):
-        print('Could not locate ', mei)
-        continue
-      # Convert to MEI4 using meigarage.
-      mei_v4 = convert324.convert(options.mei, mei_filename, options.garage)
-      with open(os.path.join(options.output, mei_filename), 'w') as output_file:
-        output_file.write(mei_v4)
+  mei_files = [f for f in listdir(options.mei) if os.path.isfile(os.path.join(options.mei, f))]
+
+  for mei_filename in mei_files:
+    mei = os.path.join(options.mei, mei_filename)
+    if (not os.path.exists(mei)):
+      print('Could not locate ', mei)
+      continue
+    # Convert to MEI4 using meigarage.
+    mei_v4 = convert324.convert(options.mei, mei_filename, options.garage)
+    with open(os.path.join(options.output, mei_filename), 'w') as output_file:
+      output_file.write(mei_v4)
