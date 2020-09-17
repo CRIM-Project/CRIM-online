5b12da8bfcb7c65556d82d90920d01a31bc0d9f1
raffazizzi <raffaeleviglianti@gmail.com>
2020-09-17 17:51:51 -0400

    dealing with model metadata as well

--- mei-processing/update324/add_metadata.py
+++ mei-processing/update324/add_metadata.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import os.path
+from copy import deepcopy
 from datetime import date
 import xml.etree.ElementTree as ET
 
@@ -18,6 +19,104 @@ MEIDECLS= """<?xml version="1.0" encoding="UTF-8"?>
 <?xml-model href="https://music-encoding.org/schema/4.0.1/mei-CMN.rng" type="application/xml" schematypens="http://relaxng.org/ns/structure/1.0"?>
 <?xml-model href="https://music-encoding.org/schema/4.0.1/mei-CMN.rng" type="application/xml" schematypens="http://purl.oclc.org/dsdl/schematron"?>
 """
+
+def apply_model_metadata(mei, metadata):
+  mei_doc = ET.parse(mei)
+  root = mei_doc.getroot()
+
+  head_el = root.find(f'{MEINS}meiHead')
+  fileDesc_el = head_el.find(f'{MEINS}fileDesc')
+
+  # title
+  titleStmt_el = fileDesc_el.find(f'{MEINS}titleStmt')
+  titleStmt_el.clear()
+  title_el = ET.SubElement(titleStmt_el, 'title')
+  title_el.text = metadata[5]
+
+  respStmt_el = ET.SubElement(titleStmt_el, 'respStmt')
+
+  # composer
+  composer_el = ET.Element('persName', {
+    'role': 'composer',
+    'auth': 'VIAF',
+    'auth.uri': metadata[3]
+  })
+  composer_el.text = metadata[2]
+  respStmt_el.append(composer_el)
+
+  # editors
+  editors = metadata[18].split('|')
+  for editor in editors:
+    ET.SubElement(respStmt_el, 'persName', {
+    'role': 'editor'
+  }).text = editor.strip()
+  # pubStmt  
+  pubStmt_el = fileDesc_el.find(f'{MEINS}pubStmt')
+  pubStmt_el.clear()
+  pubStmt_el.append(ET.fromstring("""<publisher>
+      Citations: The Renaissance Imitation Mass Project
+  </publisher>"""))
+  for distributor in metadata[25].split('|'):
+    pubStmt_el.append(ET.fromstring(f'<distributor>{distributor}</distributor>'))
+  pubStmt_el.append(ET.fromstring(f'<date isodate="{ISODATE}"/>'))
+  pubStmt_el.append(ET.fromstring(f'<availability>{metadata[24]}</availability>'))
+
+  # appInfo
+  appInfo_el = head_el.find(f'{MEINS}encodingDesc/{MEINS}appInfo')
+  application = """<application version="1.0.0">
+      <name>add_metadata.py</name>
+  </application>"""
+  appInfo_el.append(ET.fromstring(application))
+
+  # work data
+  worklist_el = head_el.find(f'{MEINS}workList')
+  work_el = worklist_el.find(f'{MEINS}work')
+  work_el.remove(work_el.find(f'{MEINS}incip'))
+  work_el.find(f'{MEINS}title').text = metadata[5]
+  ET.SubElement(work_el, 'composer').append(deepcopy(composer_el))
+  classification = f'<classification><termList><term>{metadata[7].strip()}</term></termList></classification>'
+  work_el.append(ET.fromstring(classification))
+
+  # source data
+  manifestation = f"""  <manifestationList xmlns="{MEINSURI}" xml:id="manifestationList">
+      <manifestation>
+        <identifier type="RISM">{metadata[20]}</identifier>
+        <titleStmt>
+          <title>{metadata[5]}</title>
+        </titleStmt>
+        <pubStmt>
+          <publisher>
+            <persName auth="VIAF" auth.uri="{metadata[12]}">{metadata[11]}</persName>
+          </publisher>
+          <date/>
+        </pubStmt>
+        <physLoc>
+          <repository>
+            <corpName>{metadata[22]}</corpName>
+            <settlement>{metadata[21]}</settlement>
+          </repository>
+          <identifier type="shelfmark"> {metadata[23]} </identifier>
+        </physLoc>
+      </manifestation>
+    </manifestationList>
+  """
+  manifestation_el = ET.fromstring(manifestation)
+  head_el.append(manifestation_el)
+
+  if (len(metadata[14].strip()) > 1):
+    # add second publisher
+    manifestation_el.find(f'{MEINS}manifestation//{MEINS}publisher').append(
+      ET.fromstring(f'<persName auth="VIAF" auth.uri="{metadata[15]}">{metadata[14]}</persName>')
+    )
+
+  # pub date
+  date_el = manifestation_el.find(f'{MEINS}manifestation/{MEINS}pubStmt/{MEINS}date')
+  if len(metadata[17].strip()) == 4:
+    date_el.attrib['isodate'] = metadata[17].strip()
+  else:
+    date_el.text = metadata[17]
+
+  return ET.tostring(root, encoding='unicode')
 
 def apply_metadata(mei, metadata):
   mass_name = "_".join(metadata[0].split('_')[0:3])
@@ -207,3 +306,20 @@ with open(METADATA_MASSES, newline='') as csvfile:
     with open(mei+'-v4', 'w') as mei_output:
       mei_output.write(MEIDECLS)
       mei_output.write(apply_metadata(mei, row))
+
+# Now deal with models
+
+with open(METADATA_MODELS, newline='') as csvfile:
+  csv_data = csv.reader(csvfile)
+  metadata = list(csv_data)
+
+  for [i, row] in enumerate(metadata):
+    if i == 0 or row[0] == '': continue
+    mei_filename = row[0]
+    mei = os.path.join(MEI_LOC, mei_filename)
+    if (not os.path.exists(mei)):
+      print('Could not locate ', mei)
+      continue
+    with open(mei+'-v4', 'w') as mei_output:
+      mei_output.write(MEIDECLS)
+      mei_output.write(apply_model_metadata(mei, row))
