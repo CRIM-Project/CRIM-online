3fc461736e68c4bf2694073854c8632646c840e1
Micah Walter <micahjwalter@gmail.com>
2020-09-22 14:15:37 -0400

    Updated management command for caching a single piece

--- crim/management/commands/cache_pieces.py
+++ crim/management/commands/cache_pieces.py
@@ -5,8 +5,20 @@ from crim.views.piece import render_piece
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        for piece in CRIMPiece.objects.all():
-            print('Caching {}'.format(piece.piece_id))
-            for i in range(30):
-                render_piece(piece.piece_id, i+1)
+    help = 'Re-caches the score of a given piece, or all pieces if none is given.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('piece_ids', nargs='*')
+
+    def handle(self, *args, **options):
+        if not options['piece_ids']:
+            for piece in CRIMPiece.objects.all():
+                print('Caching {}'.format(piece.piece_id))
+                for i in range(30):
+                    render_piece(piece.piece_id, i+1)
+        else:
+            for piece_id in options['piece_ids']:
+                try:
+                    piece = CRIMPiece.objects.get(piece_id=piece_id)
+                except CRIMPiece.DoesNotExist:
+                    raise CommandError('Piece "%s" does not exist' % piece_id)
