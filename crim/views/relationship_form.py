74dfb221c8c9bf81857151f3317319f03aadeb76
Micah Walter <micahjwalter@gmail.com>
2021-12-17 22:41:50 -0500

    Show list of all pieces so that information can be passed to continuo

--- crim/views/relationship_form.py
+++ crim/views/relationship_form.py
@@ -6,6 +6,7 @@ import json
 from crim.forms.relationship import RelationshipForm
 from crim.forms.observation import ObservationForm
 from crim.models.definition import CRIMDefinition
+from crim.models.piece import CRIMPiece
 
 from crim.common import *
 
@@ -155,7 +156,9 @@ def get_relationship(request):
         form = RelationshipForm(initial={"definition": CURRENT_DEFINITION})
 
     return render(request, 'relationship/relationship_form.html',
-                context={'form': form,
-                        'relationship_definition': CURRENT_DEFINITION.relationship_definition,
-                        'observation_definition': CURRENT_DEFINITION.observation_definition
-                        })
+                  context={'form': form,
+                           'relationship_definition': CURRENT_DEFINITION.relationship_definition,
+                           'observation_definition': CURRENT_DEFINITION.observation_definition,
+                           'all_pieces': CRIMPiece.objects.order_by('piece_id'),
+                          },
+                 )
