9192b98ed9a69b96bfbc8ef48f32709703174fb3
nicola <nicholas.dipasquale@gmail.com>
2022-06-08 19:29:07 -0400

    Reversed up and down arrow and added sortable_header calls to relationship_detail page

--- crim/templatetags/sortable_header.py
+++ crim/templatetags/sortable_header.py
@@ -3,13 +3,15 @@ from django.utils.safestring import mark_safe
 
 register  = template.Library()
 
+UP_ARROW = '&#9650;'
+DN_ARROW = '&#9660;'
+
 @register.simple_tag(takes_context=True)
 def sortable_header(context, title, param, value, default = False):
     request = context['request']
     qparam = request.GET.get(param, '')
     if (default and qparam == '') or (qparam == value):
-        title, value = f'{title} &#9660;', f'-{value}'
+        title, value = f'{title} {UP_ARROW}', f'-{value}'
     elif qparam == f'-{value}':
-        title = f'{title} &#9650;'
+        title = f'{title} {DN_ARROW}'
     return mark_safe(f'<a href="?{param}={value}">{title}</a>')
-
